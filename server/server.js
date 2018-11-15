65dbc130cdd705404feb7b23d5b12e7ca08e25b5
shiron manuelpillai <shironm1993@gmail.com>
2018-11-15 13:24:05 +0000

    broke my script into modules and set up endpoint

---
+++ server/server.js
@@ -0,0 +1,29 @@
+//external libraries
+var express = require('express');
+var bodyParser = require('body-parser');
+//local imports
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+    //get text into model
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    //save model to databse
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e)
+    })
+
+})
+app.listen(3000, () => {
+    console.log('started listening to port 3000');
+})
