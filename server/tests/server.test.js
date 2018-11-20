9c55c801e86050034c3fb76b932da54c664a7340
shiron manuelpillai <shironm1993@gmail.com>
2018-11-20 10:06:23 +0000

    add tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,19 @@ var expect = require('expect');
 var {app} = require('./../server.js');
 var {Todo} = require('./../models/todo');
 
+//dummy todo
+
+const todos = [{
+    text:"Something to do"
+},
+{
+    text:"dinner date"
+}];
 //wiping database, so test case below works every time
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 //test cases of post todo
@@ -27,7 +37,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
                
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -46,10 +56,23 @@ describe('POST /todos', () => {
                 return done(err);
             }
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((e) => done(e));
         });
 
+    });
+});
+
+
+describe('/GET Todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done)
     });
 });
